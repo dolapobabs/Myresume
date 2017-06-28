c3aa964926149cbe89796578d240c46bb81aaba9
dolapobabs <dolapob@gmail.com>
2017-06-28 02:08:52 +0100

    My Resume

---
+++ index.php
@@ -0,0 +1,342 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>My CV |HOME</title>
+<!-- for-mobile-apps -->
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false);
+		function hideURLbar(){ window.scrollTo(0,1); } </script>
+<!-- //for-mobile-apps -->
+<link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+<link rel="stylesheet" href="font-awesome/css/font-awesome.min.css">
+<link href="css/bootstrap.css" rel="stylesheet" type="text/css" media="all" />
+<link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
+<link rel="stylesheet" href="css/swipebox.css">
+
+<!-- js -->
+<script type="text/javascript" src="js/jquery-2.1.4.min.js"></script>
+
+<!-- //js -->
+<link href='//fonts.googleapis.com/css?family=Lato:400,100,100italic,300,300italic,400italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
+<link href='//fonts.googleapis.com/css?family=Open+Sans:400,300,300italic,400italic,600,600italic,700,700italic,800,800italic' rel='stylesheet' type='text/css'>
+</head>
+	
+<body>
+	<div class="banner_body">
+		<div class="container">
+			<div class="w3ls_logo_nav">
+				<div class="w3ls_logo_nav_right">
+					<div class="sap_tabs">
+						<div class="w3ls_logo_nav_left">
+							<h1><a href="index.php">DOLAPOBABS</a></h1>
+						</div>
+						<div id="horizontalTab" style="display: block; width: 100%; margin: 0px;">
+							<div class="resp-tabs-container">
+								
+								<ul class="resp-tabs-list">
+									<li class="resp-tab-item" aria-controls="tab_item-1" id="tab_item-1">Home</li>
+									<li class="resp-tab-item" aria-controls="tab_item-2" id="tab_item-2">About Me</li>
+									<li class="resp-tab-item" aria-controls="tab_item-3" id="tab_item-3">My Work</li>
+									<li class="resp-tab-item" aria-controls="tab_item-4" id="tab_item-4">Contact</li>
+								</ul>
+								<div class="tab-1 resp-tab-content" aria-labelledby="tab_item-1">
+				<!-- home -->					
+									<div class="wthree_tab_content">
+										<img src="images/PP1.png" alt=" " class="img-responsive" />
+										<div class="wthree_tab_content_pos1">
+											<ul>
+												<li><a href="https://web.facebook.com/adedolapo.adekunle.5" <i class="fa fa-facebook"></i></a></li>
+												<li><a href="http://twitter.com/dolapobabs" <i class="fa fa-twitter"></i></a></li>
+												<li><a href="http://google.com/dolapob" <i class="fa fa-google"></i></a></li>
+												<li><a href="http://github.com/dolapobabs" <i class="fa fa-github"></i></a></li>
+											</ul>
+										</div>
+										<div class="wthree_tab_content_pos">
+											<div class="wthree_tab_content_pos_grid">
+
+												<h3>Babatunde Adedolapo A.</h3>
+												<h6>Front-End Developer, Android Developer</h6>
+												<ul class="address">
+													<li>
+														<ul class="address-text">
+															<li><b>PHONE </b></li>
+															<li>+2348138266233</li>
+														</ul>
+													</li>
+													<li>
+														<ul class="address-text">
+															<li><b>ADDRESS </b></li>
+															<li>Ibadan, Nigeria</li>
+														</ul>
+													</li>
+													<li>
+														<ul class="address-text">
+															<li><b>E-MAIL </b></li>
+															<li><a href="mailto:dolapob@mail.com"> dolapob@gmail.com</a></li>
+														</ul>
+													</li>
+													<li>
+														<ul class="address-text">
+															<li><b>WEBSITE </b></li>
+															<li><a href="http://dolapobabs.ng.tn">dolapobabs.ng.tn</a></li>
+														</ul>
+													</li>
+													</br>
+													<a href="./pdf/cv.pdf" class="btn btn-info" role="button" download="myCV">Download CV</a>
+												</ul>
+											</div>
+										</div>
+									</div>
+				<!-- //home -->
+								</div>
+								<div class="tab-2 resp-tab-content" aria-labelledby="tab_item-2">
+				<!-- about -->
+									<div class="agile_about">
+										<div class="agile_about_pos">
+											<!--<img src="images/contact2.png" class="img-rounded"  width="105" height="100" class="img-responsive" />-->
+										</div>
+									</div>
+									<div class="agileits_agile_about">
+										<h3>About Me</h3>
+										<div class="col-md-6 agileits_agile_about_left">
+									
+											<p>I'm a 300 level Computer Science Student of the Federal University of Agriculture, Abeokuta. I'm a lover of anything techy. I love to try out new things. </p>
+										</div>
+										<div class="col-md-6 agileits_agile_about_right">
+											<h4>I believe in Hardwork and consistency.</h4>
+										</div>
+										<div class="clearfix"> </div>
+										<div class="agileits_skills_grids">
+											<div class="col-md-6 agileits_skills_grid">
+												<div class="skills">
+													<div class="skill1" style="width:70%"> </div>		
+													<div class="bar">
+														<p>HTML / CSS3 </p>
+													</div>
+												</div>
+												<div class="skills skills1">
+													<div class="skill3" style="width:20%"> </div>							
+													<div class="bar">
+														<p>Javascript</p>
+													</div>
+												</div>
+												<div class="skills skills2">
+													<div class="skill4" style="width:60%"> </div>							
+													<div class="bar">
+														<p>Wordpress</p>
+													</div>
+												</div>
+												<div class="skills skills3">
+													<div class="skill5" style="width:30%"> </div>							
+													<div class="bar">
+														<p>Php</p>
+													</div>
+												</div>
+											</div>
+											<div class="col-md-6 agileits_skills_grid">
+												<div class="skills">
+													<div class="skill1" style="width:50%"> </div>		
+													<div class="bar">
+														<p>photoshop</p>
+													</div>
+												</div>
+												<div class="skills skills1">
+													<div class="skill3" style="width:95%"> </div>							
+													<div class="bar">
+														<p>multitasking</p>
+													</div>
+												</div>
+												<div class="skills skills2">
+													<div class="skill4" style="width:90%"> </div>							
+													<div class="bar">
+														<p>creative</p>
+													</div>
+												</div>
+												<div class="skills skills3">
+													<div class="skill5" style="width:98%"> </div>							
+													<div class="bar">
+														<p>responsible</p>
+													</div>
+												</div>
+											</div>
+											<div class="clearfix"> </div>
+										</div>
+									</div>
+				<!-- //about -->
+								</div>
+								<div class="tab-3 resp-tab-content" aria-labelledby="tab_item-3">
+				<!-- work -->
+									<div class="agile_about">
+										<div class="agile_about_pos">
+											<!--<img src="images/work.jpg" class="img-rounded"  width="100" height="100" class="img-responsive" />-->
+										</div>
+									</div>
+									<div class="agileits_agile_about">
+										<h3>My Work</h3>
+										<div class="agileits_agile_about_portfolio">
+											<div class="col-md-4 agileits_agile_about_portfolio_left">
+												<div class="w3_portfolio_grid view second-effect">
+													<a href="works/work1.png" class="swipebox" title="1st Android App: Bible Verse V1.0">
+														<img src="works/work1.png" alt=" " class="img-responsive" />
+														<div class="mask">
+															<p>Bible Verse V1.0</p>
+														</div>
+													</a>
+												</div>
+											</div>
+											<div class="col-md-4 agileits_agile_about_portfolio_left">
+												<div class="w3_portfolio_grid view second-effect">
+													<a href="works/work4.png" class="swipebox" title="Just Java App for Andela ">
+														<img src="works/work4.png" height="300" width="500" alt=" " class="img-responsive" />
+														<div class="mask">
+															<p>Just Java</p>
+														</div>
+													</a>
+												</div>
+											</div>
+											<div class="col-md-4 agileits_agile_about_portfolio_left">
+												<div class="w3_portfolio_grid view second-effect">
+													<a href="works/work3.png" class="swipebox" title=" ">
+														<img src="works/work3.png" alt=" " class="img-responsive" />
+														<div class="mask">
+															<p>Just Java</p>
+														</div>
+													</a>
+												</div>
+											</div>
+											<div class="clearfix"> </div>
+										</div>
+										<div class="agileits_agile_about_portfolio1">
+											<div class="col-md-12 agileits_agile_about_portfolio_left">
+												<div class="w3_portfolio_grid view second-effect">
+													<a href="works/work2.png" class="swipebox" title="Website developed for Codulab Hackathon: Credits: @Sayo_paul">
+														<img src="works/work2.png" alt=" " class="img-responsive" />
+														<div class="mask">
+															<p>Hackathon</p>
+														</div>
+													</a>
+												</div>
+											</div>
+											
+									
+											<div class="clearfix"> </div>
+										</div>
+										<!-- swipe box js -->
+										<script src="js/jquery.swipebox.min.js"></script> 
+											<script type="text/javascript">
+												jQuery(function($) {
+													$(".swipebox").swipebox();
+												});
+										</script>
+										<!-- //swipe box js -->
+									</div>
+				<!-- //work -->
+								</div>
+								
+				
+								<div class="tab-4 resp-tab-content" aria-labelledby="tab_item-4">
+				<!-- contact -->
+									<div class="agile_about">
+										<div class="agile_about_pos">
+											<!--<img src="" class="img-rounded"  width="100" height="100" class="img-responsive" />-->
+										</div>
+									</div>
+									<div class="agileits_agile_about">
+										<h3>Contact Me</h3>
+										<div class="agileits_agile_about_mail">
+											<section id='form'>
+											<form method='post' action = "">
+												<div class="col-md-6 agileits_agile_about_mail_left">
+													<input type="text" name="Name" placeholder="Name"  id='name' required="">
+													<input type="text" name="Subject" placeholder="Subject" id='subject' required="">
+												</div>
+												<div class="col-md-6 agileits_agile_about_mail_left">
+													<input type="email" name="Email" placeholder="Email" id='email' required="">
+													<input type="text" name="Phone" placeholder="Phone"id='phone' required="">
+												</div>
+												<div class="clearfix"> </div>
+												<textarea name="Message" placeholder="Message..." id='message' required=""></textarea>
+												<input name='submit' id='submit' type="submit" value="register">
+											</form>
+									
+										</div>
+									</div>
+									<div class="w3agile_map">
+										<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d7914.433208361796!2d3.838841199999999!3d7.329552699999997!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x10398c4696f9b91b%3A0xf1010bd27084c3b3!2sOluyole%2C+Ibadan!5e0!3m2!1sen!2sng!4v1498463950052" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+										<div class="agileinfo_map_color">
+											<div class="agileinfo_map_color_grid">
+												<div class="col-md-4 agileinfo_map_color_grid_left">
+													<h4><span class="glyphicon glyphicon-map-marker" aria-hidden="true"></span>Address</h4>
+													<p>Ibadan, Nigeria.</p>
+												</div>
+												<div class="col-md-4 agileinfo_map_color_grid_left">
+													<h4><span class="glyphicon glyphicon-envelope" aria-hidden="true"></span>Mail Me</h4>
+													<p><a href="mailto:dolapob@gmail.com"> dolapob@gmail.com</a></p>
+													
+												</div>
+												<div class="col-md-4 agileinfo_map_color_grid_left">
+													<h4><span class="glyphicon glyphicon-earphone" aria-hidden="true"></span>Phone</h4>
+													<p>+2348138 266 233</p>
+													
+												</div>
+												<div class="clearfix"> </div>
+											</div>
+										</div>
+									</div>
+								</div>
+			<!-- //contact -->
+							</div>
+						</div>
+						
+					</div>
+					<script src="js/easyResponsiveTabs.js" type="text/javascript"></script>
+						<script type="text/javascript">
+							$(document).ready(function () {
+								$('#horizontalTab').easyResponsiveTabs({
+									type: 'default', //Types: default, vertical, accordion           
+									width: 'auto', //auto or any width like 600px
+									fit: true   // 100% fit in a container
+								});
+							});			
+						</script>
+				</div>
+				<div class="clearfix"> </div>
+			</div>
+			
+		</div>
+		</div>
+		<div class="agileits_copyright">
+				<p>© Dolapobabs 2017 . All rights reserved	</a></p>
+			</div>
+<!-- for bootstrap working -->
+<script src="js/bootstrap.js"></script>
+<!-- //for bootstrap working -->
+<?php
+if(isset($_POST["submit"])){
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "portfolio";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "INSERT INTO contactme (name, subject, email, phone, message)
+VALUES ('".$_POST["Name"]."','".$_POST["Subject"]."','".$_POST["Email"]."','".$_POST["Phone"]."','".$_POST["Message"]."')";
+
+if ($conn->query($sql) === TRUE) {
+echo "<script type= 'text/javascript'>alert('Your message has been sent successfully');</script>";
+} else {
+echo "<script type= 'text/javascript'>alert('Error: " . $sql . "<br>" . $conn->error."');</script>";
+}
+
+$conn->close();
+}
+?>
+</body>
+</html>
